da07d623ac96788da3d292c08e9dbce1ddcd4542
codedspirit <viradvt@gmail.com>
2024-01-04 00:18:47 -0600

    first commit

---
+++ src/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args) {
+        /*
+         *4 tipos de datos
+         * Enteros
+         * Decimales
+         * Caracteres
+         * Booleanos
+         */
+
+        int version;
+        version = 9;
+        version = 10;
+
+        System.out.println(version);
+
+        int versionJava = 10;
+
+        System.out.println(versionJava);
+    }
+}
